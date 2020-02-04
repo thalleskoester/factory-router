3d1846ac2b9bf0067e37cfcd2cb0db180ad89f42
Thalles koester <thallesdella@gmail.com>
2020-02-04 05:16:54 -0300

    Add support message

--- examples/controller/Website.php
+++ examples/controller/Website.php
@@ -7,8 +7,8 @@ use CoffeeCode\Router\Router;
 /**
  * Factory Router | Class Website [ EXAMPLE ]
  *
- * @category FactoryRouter\Examples\Controllers
- * @package  Routes
+ * @category Examples\Controllers
+ * @package  FactoryRouter\Examples\Controllers
  * @author   Thalles D. koester <thallesdella@gmail.com>
  * @license  https://choosealicense.com/licenses/mit/ MIT
  * @link     https://github.com/thallesdella/factory-router
@@ -36,12 +36,24 @@ class Website extends Controller
     }
     
     /**
+     * @param array|null $data
+     *
      * @return void
      */
-    public function login(): void
+    public function login(?array $data): void
     {
+        if (isset($_SESSION['login']) || $_SESSION['login'] == true) {
+            $this->router->redirect('app.home');
+        }
+    
+        $html = '';
+        $msg = filter_var(($data['msg'] ?? ''), FILTER_SANITIZE_STRIPPED);
+        if (!empty($msg)) {
+            $html .= "{$msg}<br><br>";
+        }
+        
         $action = $this->router->route('auth.login');
-        $html = "<form action='{$action}'>
+        $html .= "<form action='{$action}'>
             <label><span>User:</span><input type='text' name='user' required></label>
             <label><span>Pass:</span><input type='password' name='pass' required></label>
             <button>Submit</button></form>";
@@ -56,6 +68,7 @@ class Website extends Controller
      */
     public function error(array $data): void
     {
-    
+        $html = "Erro {$data['code']}";
+        echo $html;
     }
 }
