3d32f2c8f08aeb4317c1d21d5bc44047e081e0d9
Thalles koester <thallesdella@gmail.com>
2020-02-04 04:01:54 -0300

    Add app controller

--- examples/controller/App.php
+++ examples/controller/App.php
@@ -2,6 +2,8 @@
 
 namespace Controllers;
 
+use CoffeeCode\Router\Router;
+
 /**
  * Factory Router | Class App [ EXAMPLE ]
  *
@@ -11,11 +13,40 @@ namespace Controllers;
  * @license  https://choosealicense.com/licenses/mit/ MIT
  * @link     https://github.com/thallesdella/factory-router
  */
-class App
+class App extends Controller
 {
+    /**
+     * Website constructor.
+     *
+     * @param Router $router
+     */
+    public function __construct(Router $router)
+    {
+        parent::__construct($router);
+        session_start();
+        
+        if (!isset($_SESSION['login']) || $_SESSION['login'] != true) {
+            $this->router->redirect('website.login');
+        }
+    }
+    
+    /**
+     * @return void
+     */
     public function home(): void
     {
+        $action = $this->router->route('app.logout');
+        $html = "Bem vindo, deseja <a title='Logout' href='{$action}'>sair</a>?";
+        echo $html;
+    }
     
+    /**
+     * @return void
+     */
+    public function logout(): void
+    {
+        $_SESSION['login'] = false;
+        $this->router->redirect('website.login');
     }
 }
     
