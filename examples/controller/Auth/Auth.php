f73f41314b275c32dc6af1a888015897f2594a90
Thalles koester <thallesdella@gmail.com>
2020-02-04 04:01:33 -0300

    Add auth controller

--- examples/controller/Auth/Auth.php
+++ examples/controller/Auth/Auth.php
@@ -2,6 +2,9 @@
 
 namespace Controllers\Auth;
 
+use CoffeeCode\Router\Router;
+use Controllers\Controller;
+
 /**
  * Factory Router | Class Auth [ EXAMPLE ]
  *
@@ -11,10 +14,29 @@ namespace Controllers\Auth;
  * @license  https://choosealicense.com/licenses/mit/ MIT
  * @link     https://github.com/thallesdella/factory-router
  */
-class Auth
+class Auth extends Controller
 {
+    /**
+     * Website constructor.
+     *
+     * @param Router $router
+     */
+    public function __construct(Router $router)
+    {
+        parent::__construct($router);
+        session_start();
+    }
+    
     public function login(): void
     {
-    
+        $user = filter_input(INPUT_POST, 'user', FILTER_DEFAULT);
+        $pass = filter_input(INPUT_POST, 'pass', FILTER_DEFAULT);
+        
+        if ($user != 'admin' || $pass != 'admin') {
+            $this->router->redirect('website.login');
+            return;
+        }
+        
+        $_SESSION['login'] = true;
     }
 }
