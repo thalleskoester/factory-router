1949e96e57df2755cc9bc4793a2c9027acfc5e87
Thalles koester <thallesdella@gmail.com>
2020-02-04 04:03:00 -0300

    Add website controller

--- examples/controller/Website.php
+++ examples/controller/Website.php
@@ -2,6 +2,8 @@
 
 namespace Controllers;
 
+use CoffeeCode\Router\Router;
+
 /**
  * Factory Router | Class Website [ EXAMPLE ]
  *
@@ -11,18 +13,47 @@ namespace Controllers;
  * @license  https://choosealicense.com/licenses/mit/ MIT
  * @link     https://github.com/thallesdella/factory-router
  */
-class Website
+class Website extends Controller
 {
+    /**
+     * Website constructor.
+     *
+     * @param Router $router
+     */
+    public function __construct(Router $router)
+    {
+        parent::__construct($router);
+    }
+    
+    /**
+     * @return void
+     */
     public function home(): void
     {
-    
+        $action = $this->router->route('website.login');
+        $html = "<a title='Fazer Login' href='{$action}'>Fazer Login</a>";
+        echo $html;
     }
     
+    /**
+     * @return void
+     */
     public function login(): void
     {
-    
+        $action = $this->router->route('auth.login');
+        $html = "<form action='{$action}'>
+            <label><span>User:</span><input type='text' name='user' required></label>
+            <label><span>Pass:</span><input type='password' name='pass' required></label>
+            <button>Submit</button></form>";
+        
+        echo $html;
     }
     
+    /**
+     * @param array $data
+     *
+     * @return void
+     */
     public function error(array $data): void
     {
     
