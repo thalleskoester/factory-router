9e08788d55720d7bb4b4d30c9ff31fac57214e17
Thalles koester <thallesdella@gmail.com>
2020-02-04 05:17:24 -0300

    Session start global

--- examples/controller/Controller.php
+++ examples/controller/Controller.php
@@ -7,8 +7,8 @@ use CoffeeCode\Router\Router;
 /**
  * Factory Router | Class Controller [ EXAMPLE ]
  *
- * @category FactoryRouter\Examples\Controllers
- * @package  Routes
+ * @category Examples\Controllers
+ * @package  FactoryRouter\Examples\Controllers
  * @author   Thalles D. koester <thallesdella@gmail.com>
  * @license  https://choosealicense.com/licenses/mit/ MIT
  * @link     https://github.com/thallesdella/factory-router
@@ -27,6 +27,7 @@ abstract class Controller
      */
     public function __construct(Router $router)
     {
+        session_start();
         $this->router = $router;
     }
 }
